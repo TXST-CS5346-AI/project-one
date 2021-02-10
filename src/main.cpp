a0595abc1e8bcc0a11a22f3ca1e9223ac40b4ef9
dTorr <dat54@txstate.edu>
2021-02-09 22:52:01 -0600

    Complete initial run - has the Position information included. Able to reach all conclusions properly.

--- src/main.cpp
+++ src/main.cpp
@@ -92,7 +92,7 @@ public:
     void runBackwardChaining();
     int findValidConclusionInStatements(string conclusionName, int startingIndex, string stringToMatch);
     bool instantiatePremiseClause(ClauseItem & clause);
-    bool processConclusion(int conclusionLocation, string valueToMatch);
+    //bool processConclusion(int conclusionLocation, string valueToMatch);
     bool processPremiseList(Statement & statement, string stringToMatch);
 
 
@@ -106,7 +106,42 @@ void BackChain::populateLists()
 {
     statementList.push_back(Statement());
     variableList.push_back(VariableListItem("Empty", false, "", "This is an error string", STRING));
-   
+    variableList.push_back(VariableListItem("DEGREE", false,"", "Do you have a degree YES or NO: ", STRING));
+    variableList.push_back(VariableListItem("DISCOVERY", false, "", "Do you have a discovery YES or NO: ", STRING));
+    variableList.push_back(VariableListItem("EXPERIENCE", false, "", "Enter years of experience: ", INT));
+    variableList.push_back(VariableListItem("GPA", false, "", "Enter your GPA: ", INT));
+
+    vector<ClauseItem> pOne;
+    pOne.push_back(ClauseItem("NULL", "NULL", STRING));
+    pOne.push_back(ClauseItem("DEGREE", "NO", STRING));
+    vector<ClauseItem> pTwo;
+    pTwo.push_back(ClauseItem("NULL", "NULL", STRING));
+    pTwo.push_back(ClauseItem("DEGREE", "YES", STRING));
+    vector<ClauseItem> pThree;
+    pThree.push_back(ClauseItem("NULL", "NULL", STRING));
+    pThree.push_back(ClauseItem("DEGREE", "YES", STRING));
+    pThree.push_back(ClauseItem("DISCOVERY", "YES", STRING));
+    vector<ClauseItem> pFour;
+    pFour.push_back(ClauseItem("NULL", "NULL", STRING));
+    pFour.push_back(ClauseItem("QUALIFY", "YES", INT));
+    pFour.push_back(ClauseItem("GPA", "2", INT));
+    pFour.push_back(ClauseItem("EXPERIENCE", "4", INT));
+    vector<ClauseItem> pFive;
+    pFive.push_back(ClauseItem("NULL", "NULL", STRING));
+    pFive.push_back(ClauseItem("QUALIFY", "YES", STRING));
+    pFive.push_back(ClauseItem("GPA", "2", INT));
+    pFive.push_back(ClauseItem("EXPERIENCE", "1", INT));
+    vector<ClauseItem> pSix;
+    pSix.push_back(ClauseItem("NULL", "NULL", STRING));
+    pSix.push_back(ClauseItem("QUALIFY", "YES", STRING));
+    pSix.push_back(ClauseItem("GPA", "4", INT));
+
+    statementList.push_back(Statement(ClauseItem("POSITION", "NO", STRING), pOne));
+    statementList.push_back(Statement(ClauseItem("QUALIFY", "YES", STRING), pTwo));
+    statementList.push_back(Statement(ClauseItem("POSITION", "RESEARCH", STRING), pThree));
+    statementList.push_back(Statement(ClauseItem("POSITION", "SERVICEENGINEER", STRING), pFour));
+    statementList.push_back(Statement(ClauseItem("POSITION", "NO", STRING), pFive));
+    statementList.push_back(Statement(ClauseItem("POSITION", "PRODUCTENGINEER", STRING), pSix));
     //from the knowledge base transform each statement as follows:
     //for each statement in the knowledge base:
     //  get the conclusion name from the statement.
@@ -117,14 +152,14 @@ void BackChain::populateLists()
     //
 
 }
-
+/*
 bool BackChain::processConclusion(int conclusionLocation, string valueToMatch)
 {
     int solution = 0;
     bool isValid = true;
     int location = 0;
 
-    for (int premiseIter = 1; (isValid && statementList.at(conclusionLocation).premiseList.size()); premiseIter++)
+    for (int premiseIter = 1; (isValid && premiseIter < statementList.at(conclusionLocation).premiseList.size()); premiseIter++)
     {
         conclusionLocation = findValidConclusionInStatements(statementList.at(conclusionLocation).premiseList.at(premiseIter).name, 1, "");
         
@@ -140,7 +175,7 @@ bool BackChain::processConclusion(int conclusionLocation, string valueToMatch)
 
     return isValid;
 }
-
+*/
 bool BackChain::processPremiseList(Statement & statement, string stringToMatch)
 {
     int solution = 0;
@@ -149,11 +184,12 @@ bool BackChain::processPremiseList(Statement & statement, string stringToMatch)
     int conclusionLocation = 0;
     string valueToMatch = "";
 
-        for (int premiseIter = 1; (isValid && statement.premiseList.size()); premiseIter++)
+    for (int premiseIter = 1; (isValid && premiseIter < statement.premiseList.size()); premiseIter++)
         {
+
             //go through the entire list and see if there is a valid one
-            conclusionLocation = findValidConclusionInStatements(statementList.at(conclusionLocation).premiseList.at(premiseIter).name, 1,
-                                 statementList.at(conclusionLocation).premiseList.at(premiseIter).value);
+            conclusionLocation = findValidConclusionInStatements(statement.premiseList.at(premiseIter).name, 1,
+                                 statement.premiseList.at(premiseIter).value);
 
             //is a conclusion but not valid
             if (conclusionLocation == -1)
@@ -162,8 +198,9 @@ bool BackChain::processPremiseList(Statement & statement, string stringToMatch)
             }
 
             //is a conclusion and valid
-            if (conclusionLocation >= 0)
+            if (conclusionLocation > 0)
             {
+
                 isValid = true;
                 //This may be partially wrong, might already have done this...
                 //valueToMatch = statementList.at(conclusionLocation).premiseList.at(premiseIter).value;
@@ -173,11 +210,11 @@ bool BackChain::processPremiseList(Statement & statement, string stringToMatch)
             //not a conclusion
             if(conclusionLocation == 0)
             {
-                isValid = instantiatePremiseClause(statementList.at(conclusionLocation).premiseList.at(premiseIter));
+                isValid = instantiatePremiseClause(statement.premiseList.at(premiseIter));
             }
-        }
+    }
 
-    return true;
+    return isValid;
 }
 
 bool BackChain::instantiatePremiseClause(ClauseItem & clause)
@@ -195,6 +232,7 @@ bool BackChain::instantiatePremiseClause(ClauseItem & clause)
             {
                 cout << variableList.at(premiseClauseIter).description;
                 cin >> variableList.at(premiseClauseIter).value;
+                variableList.at(premiseClauseIter).instantiated = true;
             }
 
             if (variableList.at(premiseClauseIter).value == clause.value)
@@ -206,7 +244,6 @@ bool BackChain::instantiatePremiseClause(ClauseItem & clause)
     }
 
     return isValid;
-
 }
 
 int BackChain::findValidConclusionInStatements(string conclusionName, int startingIndex, string stringToMatch)
@@ -215,14 +252,13 @@ int BackChain::findValidConclusionInStatements(string conclusionName, int starti
     bool isConclusion = false;
     bool isValid = false;
 
-
     for (int conclusionIter = startingIndex; (conclusionIter < statementList.size() && !isValid ); conclusionIter++)
     {
         if (conclusionName == statementList.at(conclusionIter).conclusion.name)
         {
             isConclusion = true;
             
-            if (stringToMatch != statementList.at(conclusionIter).conclusion.value || stringToMatch == "DONTCARE")
+            if (stringToMatch == statementList.at(conclusionIter).conclusion.value || stringToMatch == "DONTCARE")
             {
                 isValid = processPremiseList(statementList.at(conclusionIter), stringToMatch);
 
@@ -274,9 +310,9 @@ void BackChain::runBackwardChaining()
         }
 
         //is a conclusion and valid
-        if (conclusionLocation >= 0)
+        if (conclusionLocation > 0)
         {
-
+            cout << "Result is " << statementList.at(conclusionLocation).conclusion.value << endl;
             cout << "conclusion is valid";
         }
 
