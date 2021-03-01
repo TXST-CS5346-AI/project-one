bb84bcd4d3cdded6c27e630c8aa28e8af7027d2b
Boris <8976343+bss8@users.noreply.github.com>
2021-03-01 13:11:24 -0600

    Update Statement.hpp

--- include/Statement.hpp
+++ include/Statement.hpp
@@ -8,7 +8,12 @@
 /**
  * Represents a single line entry in the knowledge base file. 
  * 0 to n premises and 1 conclusion. A statement may be atomic (e.g., var = true). 
+ * Here is an example of a statement (single line in KB file): 
+ * {@code issue = Failure to Start ^ has_fuel = y ^ has_voltage = n : repair = Dead Battery, Change the battery.}
  * 
+ * Here, we use intermediate variable issue (which may also be a conclusion). 
+ * IF issue is equal to "Failure to Start AND (^ indicates logical AND) has_fuel is true AND 
+ *    has_voltage is false THEN repair recommendation is "Dead Battery, Change the battery."
  */ 
 class Statement
 {
