7305109313aca944c3dd3ff2e387c0377861d8e8
Boris <8976343+bss8@users.noreply.github.com>
2021-03-01 13:07:40 -0600

    Update VariableListItem.hpp

--- include/VariableListItem.hpp
+++ include/VariableListItem.hpp
@@ -6,6 +6,15 @@
 
 #include "KnowledgeBase.hpp"
 
+/**
+* A list of variables and their associated values that must accompany the knowledgeBase.txt file
+* The variablesList.csv file contains a comma delimited list of variables and their contents to display. 
+* For example: 
+* {@code is_distributor_cap_damaged,Is the distributor cap cracked or broken? (y/n),STRING} 
+* 
+* first value is the variable name, next is the description that will be displayed to the user if we need to obtain their
+* input about this variable, and last is the type. Currently all variables are of string type, as we read them in via text file. 
+*/
 class VariableListItem
 {
 public:
